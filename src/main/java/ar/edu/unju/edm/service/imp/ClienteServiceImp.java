65d2ccebcdd34617cb5683c583fc71cea6c57424
solis <solis@DESKTOP-LF50LIN>
2021-05-01 20:47:51 -0300

    Ajustes en Datos adicionales, se calculan e  imprimen en la vista
    -Cesar Solis

--- src/main/java/ar/edu/unju/edm/service/imp/ClienteServiceImp.java
+++ src/main/java/ar/edu/unju/edm/service/imp/ClienteServiceImp.java
@@ -1,6 +1,6 @@
 package ar.edu.unju.edm.service.imp;
 
-import java.time.LocalDate;
+
 import java.time.Period;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
@@ -22,49 +22,8 @@ public class ClienteServiceImp implements IClienteService{
 
 	@Override
 	public void guardarCliente(Cliente unCliente) {
-		// TODO Auto-generated method stub
-		// fecha para cumple
-		LocalDate fechaNac= unCliente.getFechaNacimiento();
-		LocalDate fechaAhora = LocalDate.now();
-		//fecha desde ultima compra
-		LocalDate fechaUlti = unCliente.getFechaUltimaCompra();
-		//sigue el orden de fecha anterior y fecha posterior (sig line)
-	
-		//--calculo edad--
-		Period periodo = Period.between(fechaNac, fechaAhora);
-		unCliente.setEdad(periodo.getYears());
+		// TODO Auto-generated method stub	
 		
-		//--calculo de tiempo desde ultima compra--
-		String datos= "T_desdeUltimaComp: ";
-		Period periodo2 = Period.between(fechaUlti, fechaAhora);
-		datos += (periodo2.getYears()+"-"+periodo2.getMonths()+"-"+periodo2.getDays()+" T_hastaCumple :");
-		
-		//--calculo hasta siguiente cumpleaños--
- 
-     
-       
-        //fecha de nc mejorada 
-        LocalDate nextBDay = fechaNac.withYear(fechaAhora.getYear());
-
-        //Si el cumpleaños ya ocurrió este año, agrega 1 año
-        if (nextBDay.isBefore(fechaAhora) || nextBDay.isEqual(fechaAhora)) {
-            nextBDay = nextBDay.plusYears(1);
-        }
-        
-        Period p = Period.between(fechaAhora, nextBDay);
-        long totalDias = ChronoUnit.DAYS.between(fechaAhora, nextBDay);
-
-       //Cuando totalDias=365 hoy es el cumpleaños
-
-        if (totalDias == 365)
-        {
-            datos += (p.getYears()+"-"+p.getMonths()+"-"+p.getDays()+" Feliz Cumple!");
-        } else
-        {
-            datos += (p.getYears()+"-"+p.getMonths()+"-"+ p.getDays()+" ");    
-        }
-
-		unCliente.setDatosAdicionales(datos);
 		listadoClientes.add(unCliente);	
 	}
 
